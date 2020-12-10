0f754c5d989d448e534ac03b313a148d44f3d710
Akash Dudhane <akashdudhane222@gmail.com>
2020-12-10 19:01:07 +0530

    Initial Commit

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+    public static void main (String args[]){
+        System.out.println("Hello from HelloWorld class");
+        System.out.println("Hello Akash");
+        int num=10;
+    }
+}
